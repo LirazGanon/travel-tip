1bfdeb44307d5feb5194f5485512ee052d71cc96
לירז גנון <lirazganon@gmail.com>
2022-11-03 09:27:04 +0200

    css services

--- js/app.controller.js
+++ js/app.controller.js
@@ -1,5 +1,6 @@
 import { locService } from './services/loc.service.js'
 import { mapService } from './services/map.service.js'
+import { cssService } from './services/css.service.js'
 
 window.onload = onInit
 window.onAddMarker = onAddMarker
@@ -211,14 +212,8 @@ function onDownloadCSV(elLink) {
     elLink.href = 'data:text/csv;charset=utf-8,' + csv
 }
 
-const themes = {
-    light: { text: '#333333', bg: '#eeeeee', break: '#555555', prColor: '#85c446' },
-    dark: { text: '#eeeeee', bg: '#333333', break: '#e5e5e5', prColor: '#82b541' },
-    warm: { text: '#333333', bg: '#eeeeee', break: '#555555', prColor: '#fdca30' },
-    cold: { text: '#eeeeee', bg: '#4682b4', break: '#2c2b2b', prColor: '#999999' },
-}
-
 function onChangeTheme(theme) {
+    const themes = cssService.getThemes()
     const curTheme = themes[theme]
     if (!curTheme) return
     document.documentElement.style.setProperty('--text', curTheme.text);
